7267041d197c1a96f6e94340bc6f52b458391203
joreer <102667171+joreer@users.noreply.github.com>
2025-02-17 21:12:19 -0400

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 const httpServer = createServer(app);
 const io = new Server(httpServer, { cors: { origin: "*" } });
 
