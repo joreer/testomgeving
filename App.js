83ede989a69585eb9f105f0b9368219017e7b8d0
joreer <102667171+joreer@users.noreply.github.com>
2025-02-17 21:00:06 -0400

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import io from "socket.io-client";
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const API_BASE_URL = process.env.REACT_APP_API_URL || "https://testomgeving.onrender.com/";
 const socket = io(API_BASE_URL);
 
 export default function App() {
